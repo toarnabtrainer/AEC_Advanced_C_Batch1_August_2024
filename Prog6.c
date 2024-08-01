be09dab4cc1ec5152619c634491137ed4c6a35ed
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-01 16:10:11 +0530

    Add files via upload

---
+++ Prog6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int choice, num1, num2, result;
+	int my_input();  // returns user given input
+	void display_result(int);  // result will be passed for display
+	int my_menu(); // menu returns 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for quit
+	int my_calculation(int, int, int) //  input parameters are num1, num2, choice
+	void my_exit();
+	
+	...
+	...
+	... complete this code with the given guidelines
+}
