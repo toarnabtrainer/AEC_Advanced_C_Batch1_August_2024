634b989573bfef204463906d39cb951e34cf6640
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-01 12:49:27 +0530

    Add files via upload

--- Prog1.c
+++ Prog1.c
@@ -4,14 +4,14 @@ int main(void) {
 	int num1, num2, total;
 	int my_input(void);
 	int my_addition(int, int);
+	void my_display(int, int, int);
+	void my_exit(void);
 	
 	num1 = my_input();
 	num2 = my_input();
 	total = my_addition(num1, num2);
-	
-	printf("\nSo %d + %d = %d...", num1, num2, total);
-	
-	printf("\n\nEnd of the program...");
+	my_display(num1, num2, total);
+	my_exit();
 }
 
 int my_input(void) {
@@ -23,4 +23,11 @@ int my_input(void) {
 int my_addition(int n1, int n2) {
 	printf("\nPerforming the addition operation...");
 	return (n1 + n2);
+}
+void my_display(int a, int b, int tot) {
+	printf("\nDisplaying the result...");
+	printf("\nSo %d + %d = %d...", a, b, tot);
+}
+void my_exit(void) {
+	printf("\n\nEnd of the program...");
 }
