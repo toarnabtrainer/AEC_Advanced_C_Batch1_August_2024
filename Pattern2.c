5929149fae82353ae452dc3296b1f172ba82e606
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-02 10:59:33 +0530

    Add files via upload

--- Pattern2.c
+++ Pattern2.c
@@ -2,9 +2,9 @@
 n = 6  (User i/p)    i    .    *
                   ----------------
 ***********          1    0   11        (n, i)
-.*********           2    1    9   . => (???)
+.*********           2    1    9   . => (i - 1)
 ..*******            3    2    7
-...*****             4    3    5   * => (???)
+...*****             4    3    5   * => (2 * (n - i) + 1)
 ....***              5    4    3
 .....*               6    5    1
                   ----------------
@@ -17,8 +17,8 @@ int main(void) {
 	printf("\nPlease enter the number of layers: ");
 	scanf("%d", &n);
 	for (i = 1; i <= n; i++) {
-		for (j = 1; j <= ???; j++) printf(".");
-		for (j = 1; j <= ???; j++) printf("*");
+		for (j = 1; j <= (i - 1); j++) printf(".");
+		for (j = 1; j <= (2 * (n - i) + 1); j++) printf("*");
 		printf("\n");
 	}
 	printf("\n\nEnd of the program...");
