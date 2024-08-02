6406e3246ccba6fe67fe1c996a89e61ba6828c20
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-02 15:33:30 +0530

    Add files via upload

---
+++ Series9.c
@@ -0,0 +1,4 @@
+//        index (i): 1   2   3   4    5   6   7   8   9   10   11  12   ... n
+// Series-1: total = 1 + 2 + 3 + 4 + 10 + 5 + 6 + 7 + 8 + 26 + 9 + 10 + ... n terms
+
+What will be the 99-th term of this series?
