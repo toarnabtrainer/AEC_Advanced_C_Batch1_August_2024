f204cff3c8873bd009cf6a7076df549878f88015
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-02 11:17:49 +0530

    Add files via upload

--- Pattern4.c
+++ Pattern4.c
@@ -18,7 +18,7 @@ int main(void) {
 	scanf("%d", &n);
 	for (i = 1; i <= n; i++) {
 		for (j = 1; j <= (i - 1); j++) printf(".");
-		for (j = 1; j <= (2 * (n - i) + 1); j++) printf(????);
+		for (j = 1; j <= (2 * (n - i) + 1); j++) printf("%c", 64 + j);
 		printf("\n");
 	}
 	printf("\n\nEnd of the program...");
