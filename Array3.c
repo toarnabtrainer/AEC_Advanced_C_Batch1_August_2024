82198b6d7d85a1b35d2438281ff6296c8286200d
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-03 10:57:27 +0530

    Add files via upload

--- Array3.c
+++ Array3.c
@@ -1,24 +1,25 @@
-
+/*
 Array Fill up:
 maxloc = 5
 	        col
-	   0   1   2   3   4
---------------------------
-r 0 |  10  0   0   0  40  
-o 1 |  0  10   0  40   0  
-w 2 |  0   0  50   0   0  
-  3 |  0  40   0  10   0  
-  4 | 40   0   0   0  10  
---------------------------
+	   0   1   2   3   4     row   col
+--------------------------  -----------
+r 0 |  10  0   0   0  40       0     4
+o 1 |  0  10   0  40   0       1     3  col => (maxloc - row - 1)
+w 2 |  0   0  50   0   0       2     2
+  3 |  0  40   0  10   0       3     1
+  4 | 40   0   0   0  10       4     0
+--------------------------  ------------
 */
 #include <stdio.h>
+int arr[5][5];
 int main(void) {
-	int arr[5][5], row, col, maxloc = 5;
+	int row, col, maxloc = 5;
 	printf("\nInitializing the content of the array...");
 	for (row=0; row<maxloc; row++) {
 		for (col=0; col<maxloc; col++) {
 			if (row == col) arr[row][col] = 10;
-			else arr[row][col] = 0;
+			if (row + col == maxloc - 1) arr[row][col] += 40;
 		}
 	}
 	printf("\n\nDisplaying the content of the array...\n");
