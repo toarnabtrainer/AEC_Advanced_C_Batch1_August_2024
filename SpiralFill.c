225fe9e4c712f18cc8e4c0b6eddb589a0b125e55
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-03 14:54:50 +0530

    Add files via upload

---
+++ SpiralFill.c
@@ -0,0 +1,16 @@
+Spiral Fill Up:
+---------------
+Fill up a square matrix with dimension nxn (where n is one user given integer) with numbers
+ranging from 1 to n*n using them once in a clockwise spiral manner starting from top-left
+corner of the matrix. As example -
+
+n = 5 (user given integer)
+      0   1   2   3   4
+--------------------------
+ 0 |  1   2   3   4   5
+ 1 | 16  17  18  19   6
+ 2 | 15  24  25  20   7
+ 3 | 14  23  22  21   8
+ 4 | 13  12  11  10   9
+--------------------------
+
