fe528a33c675198a5ca0ce4bc9b1ec59788d18c2
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-03 16:37:48 +0530

    Add files via upload

---
+++ MultiplyBy3.c
@@ -0,0 +1,10 @@
+Find all possible those numbers which when multiply by 3 will produce the product number
+as output whether the first digit of the source number at the last in the product and 
+all other digits will maintain the same sequence.
+As example -
+
+        xy......z
+              x 3
+        ---------
+        y......zx
+     
