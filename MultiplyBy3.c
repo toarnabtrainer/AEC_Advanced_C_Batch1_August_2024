c48dbd0bed9a4cc8cd27d2d880f8f62247d2d3b8
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-03 16:52:02 +0530

    Add files via upload

--- MultiplyBy3.c
+++ MultiplyBy3.c
@@ -2,9 +2,9 @@ Find all possible those numbers which when multiply by 3 will produce the produc
 as output whether the first digit of the source number at the last in the product and 
 all other digits will maintain the same sequence.
 As example -
-
-        xy......z
-              x 3
-        ---------
-        y......zx
+                         1 0 2 1 2
+        xy......z        1 4 2 8 5 7
+              x 3                x 3
+        ---------      -------------
+        y......zx        4 2 8 5 7 1
      
