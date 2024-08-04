5c931217749f62f790254653905466fe81a00eaa
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-04 14:38:04 +0530

    Add files via upload

--- ArrPointer1.c
+++ ArrPointer1.c
@@ -3,7 +3,7 @@
 int main(void) {
 	int arr[10];
 	void array_initialize(int []);
-	void array_display(int []);
+	void array_display(const int []);
 	
 	array_initialize(arr);
 	array_display(arr);
@@ -11,11 +11,12 @@ int main(void) {
 	printf("\n\nEnd of the program...");
 }
 
-void array_display(int y[]) {
+void array_display(const int *y) {   // int y[], int y[10], int y[50], int y[5], int *y
 	int j;
 	printf("\n\nDisplaying the current content of the array...");
+	// y[5] = 555;
 	for (j = 0; j < 10; j++) {
-		printf("\nLocation number: %4d and Content: %4d...", j, y[j]);
+		printf("\nLocation number: %4d and Content: %4d...", j, *(j + y));  // y[j], *(y + j), *(j + y), j[y]
 	}
 }
 
