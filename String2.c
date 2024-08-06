ee285cf2cfce4bbfc52f573af91c163b2b90105d
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-06 11:50:45 +0530

    Add files via upload

--- String2.c
+++ String2.c
@@ -15,15 +15,27 @@ int main(void) {
 	printf("\n\nThe string in the upper case is %s...", my_str_strupr(str));
 	printf("\n\nThe string in the upper case is %s...", my_str_strlwr(str));
 	my_str_display(str);
-	printf("\nOutcome of comparing two string is %d...", my_str_strcmp("amit", "amit"));  // 0
-	printf("\nOutcome of comparing two string is %d...", my_str_strcmp("amitava", "amit"));  // 1
-	printf("\nOutcome of comparing two string is %d...", my_str_strcmp("amit", "amitava"));  // -1
-	printf("\nOutcome of comparing two string is %d...", my_str_strcmp("Amit", "amit"));  // -1
-	printf("\nOutcome of comparing two string is %d...", my_str_strcmp("amit", "kamal"));  // -1
-	printf("\nOutcome of comparing two string is %d...", my_str_strcmp("X", "kamal"));  // -1
+	printf("\nOutcome of comparing two strings is %d...", my_str_strcmp("amit", "amit"));  // 0
+	printf("\nOutcome of comparing two strings is %d...", my_str_strcmp("amitava", "amit"));  // 1
+	printf("\nOutcome of comparing two strings is %d...", my_str_strcmp("amit", "amitava"));  // -1
+	printf("\nOutcome of comparing two strings is %d...", my_str_strcmp("Amit", "amit"));  // -1
+	printf("\nOutcome of comparing two strings is %d...", my_str_strcmp("amit", "kamal"));  // -1
+	printf("\nOutcome of comparing two strings is %d...", my_str_strcmp("X", "kamal"));  // -1
 	
 	printf("\n\nEnd of the program...");
 }
+
+int my_str_strcmp(const char *st1, const char *st2) {
+	int diff, i = -1;
+	while (i == -1 || (*(st1 + i) != '\x0' && *(st2 + i) != '\x0')) {
+		i++;
+		diff = *(st1 + i) - *(st2 + i);
+		if (diff != 0) break;
+	}
+	if (diff == 0) return 0;
+	return (diff < 0)? -1: 1;
+}
+
 /*
 ASCII Codes: American Standard Code for Information Interchange Codes
 8 bit codes: ranging from 0 to 255 (2^8 - 1)
@@ -79,6 +91,3 @@ void my_str_display(const char *st) {
 	printf("...");
 }
 
-int my_str_strcmp(const char *st1, const char *st2) {
-	....
-}
